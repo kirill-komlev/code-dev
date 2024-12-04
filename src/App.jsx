3ee57d246399f2d0e04b24a78a88ef244843efa9
kirill-komlev <141944537+kirill-komlev@users.noreply.github.com>
2024-12-04 13:15:57 +0000

    Сделал роуты по сайту

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,163 @@ export default function App() {
 					/>
 				</Routes>
 
-				<main>456</main>
+				<main>
+					<Routes>
+						<Route
+							exact
+							path='/'
+							element={
+								<>
+									<br />
+									<Link to='courses'>- курсы</Link>
+									<br />
+								</>
+							}
+						/>
+						<Route path='courses'>
+							<Route
+								exact
+								index
+								element={
+									<>
+										страница курсы
+										<br />
+										<Link to='html-css'>- html css</Link>
+										<br />
+										<Link to='javascript'>- javascript</Link>
+										<br />
+										<Link to='php'>- php</Link>
+										<br />
+										<Link to='git'>- git</Link>
+										<br />
+										<Link to='..'>- назад</Link>
+									</>
+								}
+							/>
+							<Route path='html-css'>
+								<Route
+									exact
+									index
+									element={
+										<>
+											страница html css
+											<br />
+											<Link to='бэм'>- БЭМ</Link>
+											<br />
+											<Link to='adaptive'>- адаптив</Link>
+											<br />
+											<Link to='semantic'>- семантика</Link>
+											<br />
+											<Link to='..'>- назад</Link>
+										</>
+									}
+								/>
+								<Route
+									exact
+									path='бэм'
+									element={
+										<>
+											страница бэм
+											<br />
+											<Link to='..'>- назад</Link>
+										</>
+									}
+								/>
+								<Route
+									exact
+									path='adaptive'
+									element={
+										<>
+											страница adaptive
+											<br />
+											<Link to='..'>- назад</Link>
+										</>
+									}
+								/>
+								<Route
+									exact
+									path='semantic'
+									element={
+										<>
+											страница semantic
+											<br />
+											<Link to='..'>- назад</Link>
+										</>
+									}
+								/>
+							</Route>
+							<Route path='javascript'>
+								<Route
+									exact
+									index
+									element={
+										<>
+											страница javascript
+											<br />
+											<Link to='react'>- React</Link>
+											<br />
+											<Link to='nodejs'>- NodeJs</Link>
+											<br />
+											<Link to='..'>- назад</Link>
+										</>
+									}
+								/>
+								<Route
+									exact
+									path='react'
+									element={
+										<>
+											страница React
+											<br />
+											<Link to='..'>- назад</Link>
+										</>
+									}
+								/>
+								<Route
+									exact
+									path='nodejs'
+									element={
+										<>
+											страница NodeJs
+											<br />
+											<Link to='..'>- назад</Link>
+										</>
+									}
+								/>
+							</Route>
+							<Route
+								exact
+								path='php'
+								element={
+									<>
+										страница php
+										<br />
+										<Link to='..'>- назад</Link>
+									</>
+								}
+							/>
+							<Route
+								exact
+								path='git'
+								element={
+									<>
+										страница git
+										<br />
+										<Link to='..'>- назад</Link>
+									</>
+								}
+							/>
+						</Route>
+					</Routes>
+				</main>
 
-				<footer></footer>
+				<Routes>
+					<Route
+						exact
+						path='*'
+						element={<footer className='footer'>подвал</footer>}
+					/>
+				</Routes>
 			</Suspense>
 		</>
 	)
