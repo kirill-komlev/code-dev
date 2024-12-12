775a83840d51c12ebf5260cf50451bb75af10b64
Kirill Komlev <kirillkomlev02@gmail.com>
2024-12-12 20:48:42 +0300

    Сделал блок почему выбирают нас

--- src/assets/page/MainPage/MainPage.jsx
+++ src/assets/page/MainPage/MainPage.jsx
@@ -3,8 +3,34 @@ import { Link } from 'react-router-dom'
 import Intro from '../../components/Intro/Intro'
 
 import { logo } from '../../../../config'
+import { why_us } from '../../data/data'
 
 export default function MainPage() {
+	function WhyUs__block({ data }) {
+		return (
+			<>
+				<div className='why-us__block'>
+					<h3 className='why-us__block-title'>{data.title}</h3>
+					<p className='why-us__block-text'>{data.text}</p>
+				</div>
+			</>
+		)
+	}
+
+	function WhyUs__block__render() {
+		let data = []
+		why_us.forEach(element => {
+			data.push(
+				<WhyUs__block
+					key={element.title}
+					data={element}
+				></WhyUs__block>
+			)
+		})
+
+		return data
+	}
+
 	return (
 		<>
 			<Intro>
@@ -25,11 +51,14 @@ export default function MainPage() {
 			</Intro>
 			<section className='why-us'>
 				<div className='why-us__inner container'>
-					<h2 className='why-us__title'>Почему выбирают именно нашу онлайн-школу по веб-разработке?</h2>
+					<h2 className='why-us__title'>Почему выбирают именно нашу онлайн-школу?</h2>
 					<p className='why-us__text'>
 						Наша онлайн-школа предлагает качественное обучение веб-разработке, которое позволит вам стать востребованным специалистом в этой области. Мы гарантируем, что наши
 						выпускники получают необходимые знания и навыки для успешной карьеры.
 					</p>
+					<div className='why-us__blocks'>
+						<WhyUs__block__render />
+					</div>
 				</div>
 			</section>
 		</>
